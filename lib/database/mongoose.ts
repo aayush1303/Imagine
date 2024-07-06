eae197d85c5931a5f73a053ee026fcc6d3a73121
aayush1303 <aayushpanwar5177@gmail.com>
2024-07-06 22:14:04 +0530

    initial commit

---
+++ lib/database/mongoose.ts
@@ -0,0 +1,35 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose
+
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
+}
+
+export const connectToDatabase = async () => {
+  if(cached.conn) return cached.conn;
+
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imagine', bufferCommands: false 
+    })
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
+
+
+//nextjs runs in serverless mode , so we need to send request again and again and we use cache to simplify the process and increase efficiency
